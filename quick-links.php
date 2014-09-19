e19e6e48dda65ccce31a96ac72528c692f10ff0a
Andrew Minion <andrew@andrewrminion.com>
2014-09-19 13:07:08 -0400

    initial plugin file

---
+++ quick-links.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Homepage Quick Links
+ * Plugin URI: http://code.andrewrminion.com/quick-links-plugin
+ * Description: Gives you “quick link” buttons on the home page”
+ * Version: 1.0
+ * Author: Andrew Minion
+ * Author URI: http://andrewrminion.com
+ * License: GPL2
+ */
+
+
+?>
