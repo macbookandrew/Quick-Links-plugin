2e37d8dd16004fda47c13c1981fad1dab296d8cd
Andrew Minion <andrew@andrewrminion.com>
2014-09-19 14:06:53 -0400

    add backend code

--- quick-links.php
+++ quick-links.php
@@ -1,13 +1,27 @@
 <?php
 /**
- * Plugin Name: Homepage Quick Links
+ * Plugin Name: Quick Links
  * Plugin URI: http://code.andrewrminion.com/quick-links-plugin
- * Description: Gives you “quick link” buttons on the home page”
+ * Description: Gives you “quick link” buttons on the home page
  * Version: 1.0
  * Author: Andrew Minion
  * Author URI: http://andrewrminion.com
  * License: GPL2
  */
 
+/* Prevent this file from being accessed directly */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/* load backend */
+if ( is_admin() ) {
+    require_once( 'inc/quick-links-admin.php' );
+}
+
+/* display frontend */
+if ( ! is_admin() ) {
+    require_once( 'inc/shortcode.php' );
+}
 
 ?>
